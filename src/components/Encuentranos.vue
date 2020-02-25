f991621c67140a0169a32df8bb679b41b7715010
Fernando Fernández Valero <ferval19@gmail.com>
2020-02-25 19:56:26 +0100

    - Metemos los estilos para que el mapa sea responsive

--- src/components/Encuentranos.vue
+++ src/components/Encuentranos.vue
@@ -1,8 +1,8 @@
 <template>
-  <section id="encuentranos" class="pt-12 pb-48">
+  <section id="encuentranos" class="pt-12 pb-24">
     <div class="container mx-auto px-4">
       <div class="flex flex-wrap">
-        <div class="w-full md:w-6/12 lg:w-6/12 lg:mb-0 mb-12 px-4">
+        <div class="w-full md:w-4/12 lg:w-6/12 lg:mb-0 mb-12 px-4">
           <div class="px-6">
             <div class="pt-6 text-center">
               <div class="flex justify-center">
@@ -16,13 +16,14 @@
                 class="mt-1 text-sm text-gray-500 uppercase font-semibold"
               >Diplomado en Relaciones Laborales</p>
               <p class="mt-1 text-sm text-gray-500 uppercase font-semibold">Graduado social Nº 784</p>
-              <p class="mt-1 text-sm text-gray-900 uppercase font-semibold">Teléfono: 678414 477</p>
+              <p class="mt-1 text-sm text-gray-900 uppercase font-semibold">Teléfono: 678 414 477</p>
+              <p class="mt-1 text-sm text-gray-900 uppercase font-semibold">Tel. Fijo: 950 390 821</p>
               <p class="mt-1 text-sm text-gray-900 font-semibold">Email: lorenzo@bariagestion.com</p>
             </div>
           </div>
         </div>
-        <div class="w-full md:w-6/12 lg:w-6/12 lg:mb-0 mb-12 px-4">
-          <div class="px-6">
+        <div class="w-full md:w-8/12 lg:w-6/12 lg:mb-0 mb-12 px-4">
+          <div class="px-6 google-maps">
             <iframe
               src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3175.9277719157863!2d-1.872153834697396!3d37.2494211298585!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd7ad502ed3fe381%3A0xcfb40f9e04ca1497!2sBARIAGESTION%20Asesoria%2FGestoria!5e0!3m2!1ses!2ses!4v1582452612107!5m2!1ses!2ses"
               width="600"
@@ -37,3 +38,19 @@
     </div>
   </section>
 </template>
+
+<style scoped>
+.google-maps {
+  position: relative;
+  padding-bottom: 75%;
+  height: 0;
+  overflow: hidden;
+}
+.google-maps iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100% !important;
+  height: 100% !important;
+}
+</style>
