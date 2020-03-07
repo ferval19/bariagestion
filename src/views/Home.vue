92763ea7b4d52a4cd7a4a456ef26d1d83189574e
Fernando Fernández Valero <ferval19@gmail.com>
2020-03-07 18:48:07 +0100

    -Añadimos el componente Hero.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,45 +2,7 @@
   <div>
     <navbar-component></navbar-component>
     <main>
-      <div
-        class="relative pt-16 pb-32 flex content-center items-center justify-center"
-        style="min-height: 75vh;"
-      >
-        <div
-          class="absolute top-0 w-full h-full bg-center bg-cover"
-          style="background-image: url(https://images.unsplash.com/photo-1496902526517-c0f2cb8fdb6a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80)"
-        >
-          <span id="blackOverlay" class="w-full h-full absolute opacity-75 bg-black"></span>
-        </div>
-        <div class="container relative mx-auto">
-          <div class="items-center flex flex-wrap">
-            <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-              <div class="pr-12">
-                <h1 class="text-white font-semibold text-5xl">Estamos cerca de tí.</h1>
-                <p
-                  class="mt-4 text-lg text-gray-300"
-                >Si tienes algún tipo de duda para gestionar tus tramites con la adminstración, Baria Gestión es tu sitio de confianza, ofreciendo un trato personalizado, así como soluciones rápidas y ágiles para tus tramites.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
-          style="height: 70px; transform: translateZ(0px);"
-        >
-          <svg
-            class="absolute bottom-0 overflow-hidden"
-            xmlns="http://www.w3.org/2000/svg"
-            preserveAspectRatio="none"
-            version="1.1"
-            viewBox="0 0 2560 100"
-            x="0"
-            y="0"
-          >
-            <polygon class="text-gray-300 fill-current" points="2560 0 2560 100 0 100" />
-          </svg>
-        </div>
-      </div>
+      <hero-component></hero-component>
       <servicios-component></servicios-component>
       <national-component></national-component>
       <enlaces-component></enlaces-component>
@@ -53,6 +15,7 @@
 </template>
 <script>
 import NavbarComponent from "../components/Navbar.vue";
+import HeroComponent from "../components/Hero.vue";
 import ServiciosComponent from "../components/Servicios.vue";
 import EmpresaComponent from "../components/Empresa.vue";
 import EncuentranosComponent from "../components/Encuentranos.vue";
@@ -64,6 +27,7 @@ export default {
   name: "home-page",
   components: {
     NavbarComponent,
+    HeroComponent,
     FooterComponent,
     EnlacesComponent,
     EmpresaComponent,
